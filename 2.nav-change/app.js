3488ff86cfb7c948e296246ab5a9a7e3e95f85fe
Adan <adanaligakhar@gmail.com>
2022-05-09 10:08:20 +0500

    section observer project nav-change

---
+++ 2.nav-change/app.js
@@ -0,0 +1,29 @@
+const header = document.querySelector('header');
+const sectionOne = document.querySelector('.home-intro');
+
+const observer = new IntersectionObserver(entries => {
+
+    // entries.forEach(entry => {
+    //     console.log(entries)
+    //     if(!entry.isIntersecting){
+    //         header.classList.add('nav-scrolled')
+
+    //     }else{
+    //         header.classList.remove('nav-scrolled')
+    //     }
+    // })
+    console.log('entries')
+
+    if(!entries[0].isIntersecting){
+
+        header.classList.add('nav-scrolled');
+    }else{
+        header.classList.remove('nav-scrolled')
+    }
+
+}, {
+    rootMargin : '-200px 0px 0px 0px '
+});
+
+
+observer.observe(sectionOne);
